3cd96bdd101e725c4eb437df2dd61f17bd8f0744
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-26 08:44:50 +0100

    Added average, averageBy and chunkBySize for List.

---
+++ TryCollections.Tests/ListTests.fs
@@ -0,0 +1,48 @@
+﻿namespace TryCollections.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type ListTests () =
+
+    [<TestMethod>]
+    member __.TryAveragePass () =
+        let result = 
+            [1.; 4.; 7.]
+            |> List.tryAverage
+        Assert.AreEqual(Some 4., result)
+
+    [<TestMethod>]
+    member __.TryAverageFail () =
+        let result = 
+            List.empty<float>
+            |> List.tryAverage
+        Assert.AreEqual(None, result)
+
+    [<TestMethod>]
+    member __.TryAverageByPass () =
+        let result = 
+            [1.; 4.; 7.]
+            |> List.tryAverageBy (fun x -> x * 2.)
+        Assert.AreEqual(Some 8., result)
+
+    [<TestMethod>]
+    member __.TryAverageByFail () =
+        let result = 
+            List.empty<float>
+            |> List.tryAverageBy (fun x -> x * 2.)
+        Assert.AreEqual(None, result)
+
+    [<TestMethod>]
+    member __.TryChunkBySizePass () =
+        let result = 
+            [1.; 4.; 7.]
+            |> List.tryChunkBySize 2
+        Assert.AreEqual(Some [[1.; 4.]; [7.]], result)
+
+    [<TestMethod>]
+    member __.TryChunkBySizeFail () =
+        let result = 
+            [1.; 4.; 7.]
+            |> List.tryChunkBySize 0
+        Assert.AreEqual(None, result)
---
+++ TryCollections/List.fs
@@ -0,0 +1,14 @@
+﻿[<RequireQualifiedAccess>]
+module List
+
+let inline tryAverage list =
+    if List.isEmpty list then None
+    else list |> List.average |> Some
+
+let inline tryAverageBy projection list =
+    if List.isEmpty list then None
+    else list |> List.averageBy projection |> Some
+
+let tryChunkBySize chunkSize list =
+    if chunkSize < 1 then None
+    else list |> List.chunkBySize chunkSize |> Some
